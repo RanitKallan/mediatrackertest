54789841662f3b858110fff94696622b87668ddf
RanitKallan <117113805+RanitKallan@users.noreply.github.com>
2026-01-30 17:19:25 -0500

    Add files via upload

--- login/signup/JS/firebase-init.js
+++ login/signup/JS/firebase-init.js
@@ -1,12 +1,25 @@
-// firebase-init.js (shared by login + signup + anime pages)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const firebaseConfig = {
-  // ✅ paste your firebaseConfig from Firebase console here
+  apiKey: "AIzaSyB4zuY75vn1Bd3AzlOZa_lm96fwOv_f4LA",
+  authDomain: "anime-list-bd85c.firebaseapp.com",
+  projectId: "anime-list-bd85c",
+  storageBucket: "anime-list-bd85c.firebasestorage.app",
+  messagingSenderId: "885351876176",
+  appId: "1:885351876176:web:1787abbca8bf82dd46b355",
+  measurementId: "G-FR303VC5Z0"
 };
+function checkAuthState() {
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in, proceed to the website
+          window.location.href = ".../index.html";
+        } else {
+          // No user is signed in, stay on the login page
+        }
+      });
+    }
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
